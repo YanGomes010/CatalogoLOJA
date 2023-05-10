fa21d931f4d5049b1e7145f80687780ce116bc69
Yan Gomes <yangomesmd@gmail.com>
2023-05-09 22:15:14 -0300

    yah

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
 
-
 fetch("db.json").then((Response) => {
   Response.json().then((produto) => {
-    var main = document.querySelector("main");
-    var section_products = document.querySelector("#main_products")
+    var section_products = document.querySelector("#main_products");
     for (let i = 0; i < produto.produtos.length; i++) {
-      //Criando cartão de produtos
-      let card = document.createElement("div");
-      card.setAttribute("class", "card");
+      section_products.innerHTML += `
+      <div class="card">
+        <div class="box_image">
+          <img id="img" src="./images/telefone_${i}.jpg"/>
+        </div>
+        <div  class="name_image">
+        ${produto.produtos[i].Nome}
+        </div>
+        <div class="price_image">
+        ${produto.produtos[i].preco}
+        </div>
+        <button key="${produto.produtos[i].id}" class="btn_buy">COMPRAR</button>
+      </div>
+      `;
+    }
+    let btn_buy = document.getElementsByTagName("button");
 
-      //Criando caixa de imagem
-      let box_image = document.createElement("div");
-      box_image.setAttribute("class", "box_image");
+    for (let a = 0; a < btn_buy.length; a++) {
+      btn_buy[a].addEventListener("click", function () {
+        let key = this.getAttribute("key");
+        produto.produtos[key].quantidade++;
+        console.log(key)
+        atualizarCarro();
+        return false;
+      });
+    }
 
-      let image = document.createElement("img");
-      image.setAttribute("id", "img");
-      image.src = `./images/telefone_${i}.jpg`;
-
-      //Criando nome do produto
-      let name_image = document.createElement("div");
-      name_image.setAttribute("class", "name_image");
-      name_image.innerText=produto.produtos[i].Nome;
-
-      //Criando preço da imagem
-      let price_image = document.createElement("div");
-      price_image.setAttribute("class", "price_image");
-      price_image.innerText= produto.produtos[i].preco;
-
-      let button = document.createElement("button");
-      button.setAttribute("class", "btn_buy");
-      button.innerText="COMPRAR"
-
-      box_image.appendChild(image);
-      card.appendChild(box_image);
-      card.appendChild(name_image);
-      card.appendChild(price_image);
-      card.appendChild(button);
-
-      section_products.appendChild(card);
-      main.appendChild(section_products)
+    function atualizarCarro(){
+      
+      let carrinho = document.querySelector(".carrinho");
+    carrinho.innerHTML="";
+    produto.produtos.map((val)=>{
+      if(val.quantidade>0){
+        carrinho.innerHTML+=`
+        
+        <p>${val.Nome} e quantidade = ${val.quantidade}</p>
+        
+        `
+      }
+    })
     }
   });
 });
+
+function abrirCarrinho(){
+  let carrinho = document.querySelector(".carrinho");
+  let btn_fechar = document.querySelector("#fechar")
+  
+  carrinho.classList.remove("none")
+  btn_fechar.classList.remove("none")
+  btn_fechar.classList.add("block")
+}
+
+function fechar(){
+  let carrinho = document.querySelector(".carrinho");
+  let btn_fechar = document.querySelector("#fechar")
+  
+  
+  
+carrinho.classList.add("none")
+btn_fechar.classList.remove("block")
+btn_fechar.classList.add("none")
+
+}
